43e33a8989a7e380256f4f0704db0cf99f35fc77
Pulak Malhotra <56169176+PulakIIIT@users.noreply.github.com>
2022-11-07 13:11:59 +0530

    Added multi gate syntax

--- interp.rkt
+++ interp.rkt
@@ -52,20 +52,44 @@
      (circuit-def name qubit (map cstCircuitBodyItem->astCircuitBodyItem body))]
     [else (error "cst->ast: bad input" x)]))
 
-(define (print-gate name args)
+(define (print-one-gate name args)
   (define indices
     (for/list ([i args])
-      (~v i)))
-  (define indices-string (string-join indices ","))
-  (define final (string-append (symbol->string name) " " "q[" indices-string "]\n"))
+      (format "q[~a]" i)))
+  (define indices-string (string-join indices ", "))
+  (define final (string-append (symbol->string name) " " indices-string))
   final)
 
+(define (print-multi-gate name multi-args sz)
+  (define instances (length (car multi-args)))
+  (define gates
+    (for/list ([i (in-range instances)])
+      (define f (map car multi-args))
+      (set! multi-args (map cdr multi-args))
+      (print-one-gate name f)))
+  (string-join gates "\n"))
+
 (define (expand-args args)
-  (flatten (for/list ([i args])
-             (if (list? i) (range (car i) (add1 (cadr i))) i))))
+  (define max-len 1)
+  (for ([lst args])
+    (when (list? lst)
+      (set! max-len (max max-len (length lst)))))
+  (for/list ([lst args])
+    (cond
+      [(and (list? lst) (eq? (length lst) max-len)) lst]
+      [(number? lst) (make-list max-len lst)])))
 
 (define (strings->string strs)
   (apply string-append strs))
+
+(define (generate-args sz max-qubits)
+  (for/list ([i (range sz)])
+    (range i (+ i (add1 (- max-qubits sz))))))
+
+(define (generate-mapped-args args arg-mapping)
+  (match args
+    [(? list?) (map (lambda (arg) (generate-mapped-args arg arg-mapping)) args)]
+    [(? number?) (list-ref arg-mapping args)]))
 
 (define (generate-qasm-from-circuit-item submodules-info circuit-item arg-mapping max-qubits)
   (cases circuit-body-item
@@ -78,20 +102,27 @@
           (name args)
           (define sz (circuit-size name max-qubits submodules-info))
           (when (empty? args)
-            (set! args (range 0 max-qubits sz)))
-          (define new_args
-            (if (empty? arg-mapping) args (map (lambda (x) (list-ref arg-mapping x)) args)))
-          (if (fundamental-gate-name? name)
-              (print-gate name new_args)
-              (generate-qasm-from-name submodules-info name new_args))]))
+            (set! args (generate-args sz max-qubits)))
+          (define multi-args (expand-args args))
+          (define instances (length (car multi-args)))
+          (define gates
+            (for/list ([i (in-range instances)])
+              (define args (map car multi-args))
+              (set! multi-args (map cdr multi-args))
+              (set! args (if (empty? arg-mapping) args (generate-mapped-args args arg-mapping)))
+              (if (fundamental-gate-name? name)
+                  (print-one-gate name args)
+                  (generate-qasm-from-name submodules-info name args))))
+          (string-join gates "\n")]))
 
 (define (generate-qasm-from-name submodules-info submodule-name arg-mapping)
   (define max-qubits (car (hash-ref submodules-info submodule-name)))
   (define submodules (cdr (hash-ref submodules-info submodule-name)))
-  (define qasms (map (lambda (circuit-item)
-         (generate-qasm-from-circuit-item submodules-info circuit-item arg-mapping max-qubits))
-       submodules))
-  (strings->string qasms))
+  (define qasms
+    (map (lambda (circuit-item)
+           (generate-qasm-from-circuit-item submodules-info circuit-item arg-mapping max-qubits))
+         submodules))
+  (string-join qasms "\n"))
 
 (define (parse-submodule submodules-info)
   (lambda (submodule)
--- tester.rkt
+++ tester.rkt
@@ -2,9 +2,12 @@
 (require rackunit)
 (require "interp.rkt")
 
-(check-equal? (expand-args '((1 5) 6 7)) '(1 2 3 4 5 6 7))
-
+(check-equal? (generate-args 2 3) '((0 1) (1 2)))
+(check-equal? (expand-args '((1 2) 3 )) '((1 2) (3 3)))
+(check-equal? (generate-mapped-args '((0 1) (2 3)) '(3 2 1 0)) '((3 2) (1 0)))
+(check-equal? (generate-mapped-args '((0) (1)) '(3 2 1 0)) '((3) (2)))
 
 (for ([file (filter (lambda (s) (string-suffix? s ".rkt"))(map path->string (directory-list "tests")))])
     (define file_name (string-append "tests/" file)) 
+    (printf "Testing ~a... \n" file_name)
     (check-equal? (run-on-file file_name) (string-trim (file->string (string-append file_name ".qasm")))))

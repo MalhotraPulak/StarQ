7cc61af80f424931b7dfe9dc22c9c96c75decdee
Pulak Malhotra <56169176+PulakIIIT@users.noreply.github.com>
2022-11-29 13:29:20 +0530

    added back cli

--- cli.rkt
+++ cli.rkt
@@ -1,2 +1,26 @@
 
+#! usr/bin/env racket
+#lang racket
 (require "interp.rkt")
+(define path (make-parameter #false))
+
+;; command line parser
+(define parser
+  (command-line
+   #:usage-help
+   "Have the computer greet you!"
+
+   #:once-each
+   [("-f" "--file") PATH
+                    "Filepath to convert to cQasm"
+                    (path PATH)]
+
+   #:args () (void)))
+
+(define (get-path f)
+  (cond
+    [(boolean? f) "Use --file argument"]
+    [(string? f) (run-on-file f)]))
+
+;; prints result to the command line
+(printf "~a\n" (get-path (path)))
